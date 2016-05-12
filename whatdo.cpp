398ee1a9390a71ac90adc48a2539b6dddbc3a2d7
nrfowler <nrfowler@gmail.com>
2016-05-12 15:07:59 -0500

    removed comments and added some classes

--- whatdo.cpp
+++ whatdo.cpp
@@ -15,6 +15,8 @@ vector<Task> readTasks();
 vector<Task> sortTasks(vector<Task> vec); 
 void addTask(){}; 
 void rmTask(){};
+void saveSchedule(){};
+void loadSchedule(){};
 duration mins2duration(int mins){ //does cpp have tio
     duration temp(mins/60,mins%60);
     return temp;
@@ -57,42 +59,22 @@ vector<Task> readTasks(){
     temptask.desc="0";
     temptask.utils=1;
     temptask.min=30;
-    temptask.outside=1;
+    temptask.indoors=1;
     temptask.sedentary=1;
     temptask.sleepfriendly=1;
     temptask.domain="NA";
-  // The array must be big enough to fit the input data.
   int row, col;
-  char buff[BUFFSIZE]; // a buffer to temporarily park the data
+  char buff[BUFFSIZE]; 
   ifstream infile("input.txt");
   stringstream ss;
-  // Read input into the buffer a line at a time, until
-  //  end of file is reached (newlines are automatically
-  //  discarded).  The buffer must be big enough to fit
-  //  an entire line from the file.
-  // Notice that while reading from the file we check how
-  //  many rows have been read, to avoid writing beyond
-  //  the end of the array.
+
   row = 0;
   while( infile.getline( buff,  BUFFSIZE ) && row < ROWS ) {
-	// copy the entire buffered line into the stringstream
 	ss << buff;
-	// Read from ss back into the buffer.  Now, ',' is
-	//  specified as the delimiter so it reads only until
-	//  it reaches a comma (which is automatically
-	//  discarded) or reaches the 'eof', but of course
-	//  this 'eof' is really just the end of a line of the
-	//  original input.  The "10" means this will handle
-	//  input numbers of 9 digits or less.
-	//  While reading from the stringstream, we check
-	//  how many columns have been read to avoid
-	//  writing past the end of the array.
+
 	col = 0;
-    //cout << buff << endl;
 	while( ss.getline( buff, 200, ',' ) && col < COLS ) {
-	  // Next, use the stdlib atoi function to convert the
-	  //  input value that was just read from ss to an int,
-	  //  and copy it into the array.
+
       if(col==0) 
       {temptask.what=buff;}
       else if(col==1) 
@@ -102,7 +84,7 @@ vector<Task> readTasks(){
       else if(col==3) 
       {temptask.min=atoi(buff);}
       else if(col==4) 
-      {temptask.outside=atoi(buff);}
+      {temptask.indoors=atoi(buff);}
       else if(col==5) 
       {temptask.sedentary=atoi(buff);}
       else if(col==6) 
@@ -112,26 +94,19 @@ vector<Task> readTasks(){
       else if(col==8) 
       {temptask.indefinite=atoi(buff);}
       
-      //cout << col<<": "<<buff << " ";
 	  ++col;
 	}
     tasks.push_back(temptask);
-	// This copies an empty string into ss, erasing the
-	//  previous contents.
+
 	ss << "";
-	// This clears the 'eof' flag.  Otherwise, even after
-	//  writing new data to ss we wouldn't be able to
-	//  read from it.
+
 	ss.clear();
 	++row;
   }
-  // Now print the array to see the result
-  //for( int _row = 0; _row < 10; ++_row ) {
+
 	for( int _col = 0; _col < 10; ++_col ) {
-	  //cout << tasks[_col].desc << " ";
 	}
 	cout << endl;
- // }
   infile.close();
 
 
--- whatdo.h
+++ whatdo.h
@@ -1,16 +1,19 @@
 #include <vector>
 class Task{
     public:
-	string what; //present-tense description of action
-	string desc; //more detailed description of action
-	int utils; // value which determines priority of task. Higher is more important.
-	int min; //minutes duration of task
-    bool outside; // 1 = outdoors. Any timespan longer than 4 hours will require an outside task.
-    bool sedentary; //1 means any sedentary activity. Too many hours of sedentary tasks will cause a non sedentary task to be added
-    bool sleepfriendly; //1 means activity will not disrupt sleep if done 1-2 hours before sleep (reading, eating, etc)
-    string domain; //The specific domain or ability that the utils is applied to, for instance career, meditation, walking.
-    bool indefinite; //has no end time; the min is the minimum minutes to spend
-};
+        string what; //present-tense description of action
+        string desc; //more detailed description of action
+        int utils; // value which determines priority of task. Higher is more important.
+        int min; //minutes duration of task.
+        bool indoors; // 1 = indoors. Any timespan longer than 4 hours will require an outside task.
+        bool sedentary; //1 means any sedentary activity. Too many hours of sedentary tasks will cause a non sedentary task to be added
+        bool sleepfriendly; //1 means activity will not disrupt sleep if done 1-2 hours before sleep (reading, eating, etc)
+        string domain; //The specific domain or ability that the utils is applied to, for instance career, meditation, walking.
+        bool indefinite; //has no end time; the min is the minimum minutes to spend
+        void decrementMin(int completed){
+            min-=completed;
+        }
+    };
      class less_than_key
     {
         public:
@@ -26,3 +29,10 @@ class Task{
     int toMins();
     duration(int h,int m): hours(h), minutes(m){};
     };
+class Schedule{
+vector<Task> tasks;
+duration length;
+int numTasks;
+int indoors;//schedules can be created for indoor tasks or outdoor tasks
+};
+
